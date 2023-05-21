183c326675791dec219d9439fabb7225e63eb339
Prakhar Shukla <imprakharshukla@gmail.com>
2023-05-21 12:15:14 +0530

    add: shit

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -2,11 +2,12 @@
 
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle,} from "@/components/ui/card"
 import {Button} from "@/components/ui/button";
-import {CgSpinner} from "react-icons/cg";
 import {useState} from "react";
 import {addDoc, collection, getFirestore} from "@firebase/firestore";
 import firebase from "@/lib/firebase";
 import {formatISO} from "date-fns";
+import {CgSpinner} from "react-icons/cg";
+import {useRouter} from "next/navigation";
 
 interface Product {
   id: number
@@ -21,6 +22,7 @@ interface Product {
 
 const ProductCard = ({product}: { product: Product }) => {
   const [loading, setLoading] = useState<boolean>(false)
+  let router = useRouter()
   // @ts-ignore
   return (
     <Card>
@@ -29,30 +31,19 @@ const ProductCard = ({product}: { product: Product }) => {
         <CardDescription>{product.description}</CardDescription>
         <p>₹ {product.price}</p>
       </CardHeader>
-      <CardContent>
-        {!loading &&
-          <img src={product.image}
-               alt={"Product"}
-               className={"h-64 w-full rounded-lg object-cover"}
-          />}
-        {loading &&
-          <CgSpinner/>
-        }
+      <CardContent className={"flex flex-col justify-center justify-center"}>
+
+        <img src={product.image}
+             alt={"Product"}
+             className={"h-64 w-full rounded-lg object-cover"}
+
+        />
       </CardContent>
       <CardFooter>
-        {/*<p>Best Before: {product.expiry.toLocaleDateString("en-US", {*/}
-        {/*  year: "numeric",*/}
-        {/*  month: "2-digit",*/}
-        {/*  day: "2-digit"*/}
-        {/*})}</p>*/}
-        {/*<p>Manufactured: {product.manufacturer_date.toLocaleDateString("en-US", {*/}
-        {/*  year: "numeric",*/}
-        {/*  month: "2-digit",*/}
-        {/*  day: "2-digit"*/}
-        {/*})}</p>*/}
+
         <Button onClick={() => {
           setLoading(true)
-          setTimeout(() => {
+          setTimeout(async () => {
             setLoading(false)
             //add the data to firebase
 
@@ -61,19 +52,20 @@ const ProductCard = ({product}: { product: Product }) => {
               // @ts-ignore
               timeZone: 'Asia/Kolkata',
             })
-            addDoc(collection(getFirestore(firebase!), 'orders'), {
+            await addDoc(collection(getFirestore(firebase!), 'orders'), {
               items: `1${product.name}`,
               status: "Received",
               timestamp: d,
               image: product.image,
               total: product.price,
               user_id: "gUXNV1Numbhay0qklszAzAFVfVP2"
-
-
             })
+            router.push("/orders")
           }, 3000)
         }
-        } className={"w-full"} size={"lg"}>Buy Now</Button>
+        } className={"w-full flex justify-center items-center"} size={"lg"}>{!loading ? "Buy Now" : (
+          <CgSpinner className={"text-2xl animate-spin"}/>
+        )}</Button>
       </CardFooter>
     </Card>
   )
