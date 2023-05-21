859e27104065570d0d2e47e368331cb581e46e5e
Hardik Prakash <hardikprakash.official@gmail.com>
2023-05-21 08:39:26 +0530

    fix: stuff

--- app/orders/data-tables.tsx
+++ app/orders/data-tables.tsx
@@ -95,8 +95,22 @@ export function DataTable<TData, TValue>({
                             <p className={"text-lg"}>{
                               // @ts-ignore
                               row.getValue("items").split(",").map((item: string, index: number) => {
-                                return <p
-                                  className={"text-md"}>{index + 1}. {item.substring(1)} x {item.substring(0, 1)}</p>
+                                return (
+                                  // <p className={"text-md"}>{index + 1}. {item.substring(1)} x {item.substring(0, 1)}</p>
+                                  <div className={"w-full bg-gray-100 dark:bg-gray-800 my-2 p-2 rounded-lg flex flex-row"}>
+                                    <img src={
+                                      item.substring(1).toLowerCase().includes('cola')
+                                        ? 'cock.jpg'
+                                        : item.substring(1).toLowerCase().includes('sprite')
+                                          ? 'sproot.jpg'
+                                          : ''
+                                    }
+                                         alt={"img"}
+                                         className={"h-24 w-24 rounded-lg object-cover"}
+                                    />
+                                    <p className={"ml-2 text-black dark:text-white"}>{item.substring(1)} <span className={"text-sm"}><br/>x{item.substring(0, 1)}</span></p>
+                                  </div>
+                                )
                               })
                             }
                             </p>
